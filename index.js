38540a29a0657072c73b07891641061387280b5a
Ryan Flynn <ryanflynn44@gmail.com>
2020-08-29 16:23:35 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+
+
+const titleCased = (input) => {
+  return tutorials.map( str => {
+    let words = str.split(' ')
+    let capitalizedWords = words.map( word => word.charAt(0).toUpperCase() + word.slice(1) )
+    let result = capitalizedWords.join(' ')
+    return result;
+  })
 }
+
+
